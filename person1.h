e6e6c4bd6552794910b84cbc0ea8e22e11212c2b
cthiel2 <clt-op@web.de>
2017-02-27 18:59:55 +0100

    Add files via upload

--- person1.h
+++ person1.h
@@ -1,16 +1,16 @@
 class person
    {
 private:
-
 public:
-    char vorname[100];
-    char nachname[100];
-    char patronym[100];
-    char middlename[100];
-    char titel[100];
-    char herkunft;
-    void askEnglish(struct person *partner);
-    void frageDeutsch(struct person *partner);
-    void greetEnglish(struct person *partner);
-    void gruesseDeutsch(struct person *partner);
+   char vorname[100];
+   char nachname[100];
+   char patronym[100];
+   char middlename[100];
+   char titel[100];
+   char herkunft;
+
+    void askEnglish(class person *partner);
+    void frageDeutsch(class person *partner);
+    void greetEnglish(class person *partner);
+    void gruesseDeutsch(class person *partner);
 };
