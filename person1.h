1092671fd5655b192d15d6892700cab03a5835d4
MarvinBusch <marvin.busch@live.de>
2017-02-27 18:37:11 +0100

    Update person1.h

--- person1.h
+++ person1.h
@@ -2,13 +2,13 @@ class person
    {
 private:
 
-   char vorname[100];
-   char nachname[100];
-   char patronym[100];
-   char middlename[100];
-   char titel[100];
-   char herkunft;
 public:
+    char vorname[100];
+    char nachname[100];
+    char patronym[100];
+    char middlename[100];
+    char titel[100];
+    char herkunft;
     void askEnglish(struct person *partner);
     void frageDeutsch(struct person *partner);
     void greetEnglish(struct person *partner);
